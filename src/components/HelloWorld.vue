b7eb5741397b5157e5c120adcf9eb05f9dc16f4b
abetomo <abe@enzou.tokyo>
2019-06-25 20:02:50 +0900

    Implement parsing with `markdown-it`
    * Add `markdown-it`
    * Select the parser to use with `markdown-it` or `Showdown`
    * Measuring time to parse

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <ul>
+      <li>
+        <input type="radio" id="showdown" value="showdown" v-model="use_lib">
+        <label for="showdown">Showdown</label>
+      </li>
+      <li>
+        <input type="radio" id="markdown-it" value="markdown-it" v-model="use_lib">
+        <label for="markdown-it">markdown-it</label>
+      </li>
+    </ul>
     <textarea rows="20" v-model="markdown"></textarea>
     <div v-html="html"></div>
   </div>
@@ -7,18 +17,34 @@
 
 <script>
 import showdown from 'showdown'
+import MarkdownIt from 'markdown-it'
 const converter = new showdown.Converter()
+const md = new MarkdownIt()
+
+const showdownConverter = (markdown) => {
+  return converter.makeHtml(markdown)
+}
+
+const markdownItConverter = (markdown) => {
+  return md.render(markdown)
+}
 
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      markdown: ''
+      markdown: '',
+      use_lib: 'showdown'
     }
   },
   computed: {
     html() {
-      return converter.makeHtml(this.markdown)
+      const converter = this.use_lib === 'showdown' ? showdownConverter : markdownItConverter
+      console.log(`use_lib: ${this.use_lib}`)
+      console.time('converter.makeHtml')
+      const html = converter(this.markdown)
+      console.timeEnd('converter.makeHtml')
+      return html
     }
   }
 }
